1968b8f9f5fb4d1fb34cdca042d57e1c39e83adf
tekogene <96535721+tekogene@users.noreply.github.com>
2022-02-05 01:03:09 +0200

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,41 @@
 function mergeArrays(left: number[], right: number[]): number[] {
-  const result = [],
-    lLen = left.length,
-    rLen = right.length;
+  const result: number[] = [];
+  const index = [0, 0];
+  let merging = true;
 
-  let l = 0,
-    r = 0;
+  const merge = (l: number, r: number) => {
+    const hasLeft = l !== undefined;
+    const hasRight = r !== undefined;
 
-  while (l < lLen && r < rLen) {
-    if (left[l] < right[r]) {
-      result.push(left[l++]);
-    } else {
-      result.push(right[r++]);
+    if (!hasLeft && !hasRight) {
+      return (merging = false);
     }
+
+    const pushLeft = (hasLeft && !hasRight) || l < r;
+
+    result.push(pushLeft ? l : r);
+    index[pushLeft ? 0 : 1] += 1;
+  };
+
+  while (merging) {
+    merge(left[index[0]], right[index[1]]);
   }
 
-  return result.concat(left.slice(l)).concat(right.slice(r));
+  return result;
 }
 
 export function mergeSort(arr: number[]): number[] {
-  const len = arr.length;
-
-  if (len < 2) {
+  if (arr.length < 2) {
     return arr;
   }
 
-  const mid = Math.floor(len / 2),
-    left = arr.slice(0, mid),
-    right = arr.slice(mid);
+  const centerIndex = Math.floor(arr.length / 2);
 
-  return mergeArrays(mergeSort(left), mergeSort(right));
+  const left = arr.slice(0, centerIndex),
+    leftSorted = mergeSort(left);
+
+  const right = arr.slice(centerIndex),
+    rightSorted = mergeSort(right);
+
+  return mergeArrays(leftSorted, rightSorted);
 }
